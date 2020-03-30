8f42410ffab918956e6d3df8195176fbc91fb9c5
Seel <Brian.Seel@baltimorecity.gov>
2020-03-30 16:45:28 -0400

    Fixing pylint errors

--- python/towing.py
+++ python/towing.py
@@ -14,16 +14,13 @@ CREATE TABLE [dbo].[towstat_bydate](
 """
 
 import argparse
-import csv
-import datetime
 import logging
 import re
 
 from datetime import datetime, time, date, timedelta
-from collections import Counter, defaultdict
-from filelock import FileLock, Timeout
-import pyodbc
+from collections import defaultdict
 from tqdm import tqdm
+import pyodbc
 
 from namedlist import namedlist
 
@@ -63,7 +60,7 @@ class TowingData:
 
         self.cursor = conn.cursor()
 
-        conn311 = pyodbc.connect(r'Driver={SQL Server};Server=balt-sql311-prd;Database=DOT_DATA;Trusted_Connection=yes;')
+        conn311 = pyodbc.connect('Driver={SQL Server};Server=balt-sql311-prd;Database=DOT_DATA;Trusted_Connection=yes;')
         self.cursor311 = conn311.cursor()
 
         data_categories = []
@@ -126,7 +123,7 @@ class TowingData:
                                 (Release_Date_Time <= Convert(datetime, '{end_date}'))
                                 OR
                                 (Release_Date_Time >= Convert(datetime, '{end_date}'))
-                                """).format(end_date=end_date, start_date=start_date)
+                                """).format(end_date=end_date)
 
         print(restriction)
 
@@ -134,15 +131,17 @@ class TowingData:
         self.cursor.execute(
             """SELECT * FROM
             (
-            SELECT Vehicle_Release.Property_Number, Receiving_Date_Time, 
-            convert(datetime, Replace(Release_Date_Time, Convert(datetime, '1899-12-31 00:00:00.000'), GETDATE())) as Release_Date_Time,
-            Pickup_Code, Pickup_Code_Change_Date, Original_Pickup_Code, Property_Type 
-            FROM [Vehicle_Release] 
-            JOIN Vehicle_Receiving 
-            ON [Vehicle_Receiving].Property_Number = Vehicle_Release.Property_Number 
-            JOIN Vehicle_Identification 
+            SELECT Vehicle_Release.Property_Number, Receiving_Date_Time,
+            convert(datetime,
+            Replace(Release_Date_Time,
+            Convert(datetime, '1899-12-31 00:00:00.000'), GETDATE())) as Release_Date_Time,
+            Pickup_Code, Pickup_Code_Change_Date, Original_Pickup_Code, Property_Type
+            FROM [Vehicle_Release]
+            JOIN Vehicle_Receiving
+            ON [Vehicle_Receiving].Property_Number = Vehicle_Release.Property_Number
+            JOIN Vehicle_Identification
             ON [Vehicle_Receiving].Property_Number = Vehicle_Identification.Property_Number
-            ) as innertable 
+            ) as innertable
             {restriction}""".format(restriction=restriction))
         return self.cursor.fetchall()
 
@@ -173,14 +172,14 @@ class TowingData:
         return res[0][0].date()
 
     @staticmethod
-    def _is_date_zero(d):
+    def _is_date_zero(check_date):
         """
         If the date is stored as a pre-1900 date, then its really just a 'null' date
 
-        :param date: datetime.date to check for nullness
-        :return: bool - true if the date is 'null'
+        :param date: (datetime.date) to check for nullness
+        :return: (bool) true if the date is 'null'
         """
-        return d < date(1900, 12, 31)
+        return check_date < date(1900, 12, 31)
 
     def _process_events(self, receive_date, release_date, code, vehicle_type, days_offset=0):  # pylint:disable=too-many-arguments
         """
@@ -285,10 +284,10 @@ class TowingData:
         """
         if not start_date:
             start_date = date(1899, 12, 31)
-            
+
         if not end_date:
             end_date = datetime.combine(date.today(), time(23, 59, 59))
-        
+
         logging.info("write_towing")
         if len(self.date_dict) == 0:
             self.calculate_vehicle_stats(start_date, end_date)
@@ -297,25 +296,24 @@ class TowingData:
 
         for towyard_date, data_acc in sorted(self.date_dict.items()):
             towyard_date = datetime.combine(towyard_date, time())
-            if not (start_date <= towyard_date <= end_date):
-                continue 
-            
+            if not start_date <= towyard_date <= end_date:
+                continue
+
             towyard_date = towyard_date.strftime('%Y-%m-%d')
 
-            for pickup_type in TOW_CATEGORIES.values():
-                for db in ['', '_nondb']:
-                    quantity = getattr(data_acc, "{}{}_num".format(pickup_type, db))
+            for pickupcode in TOW_CATEGORIES.values():
+                for dirtbike_status in ['', '_nondb']:
+                    quantity = getattr(data_acc, "{}{}_num".format(pickupcode, dirtbike_status))
 
-                    vdays = getattr(data_acc, '{}{}_age'.format(pickup_type, db))
-                    vnum = getattr(data_acc, '{}{}_num'.format(pickup_type, db))
+                    vdays = getattr(data_acc, '{}{}_age'.format(pickupcode, dirtbike_status))
+                    vnum = getattr(data_acc, '{}{}_num'.format(pickupcode, dirtbike_status))
 
                     average = (vdays / vnum) if vnum > 0 else 0
-                    dirtbike = True if db == '' else False
-                    pickupcode = pickup_type
+                    dirtbike = dirtbike_status == ''
 
                     print((towyard_date, quantity, average, dirtbike, pickupcode))
                     data.append((towyard_date, quantity, average, dirtbike, pickupcode))
-                    
+
         self.cursor311.executemany("""
         MERGE towstat_bydate USING (
         VALUES
@@ -332,7 +330,7 @@ class TowingData:
             INSERT (date, quantity, average, dirtbike, pickupcode)
             VALUES (vals.date, vals.quantity, vals.average, vals.dirtbike, vals.pickupcode)
         OUTPUT inserted.date, inserted.quantity, inserted.average, inserted.dirtbike, inserted.pickupcode;
-        """, data )
+        """, data)
         self.cursor311.commit()
 
     @staticmethod
@@ -360,7 +358,7 @@ def main():
                               'not specified'))
     parser.add_argument('-y', '--year', type=int, default=yesterday.year,
                         help=('Optional: Year of date we should start searching on (IE: 2020). Defaults to all days '
-                               'if not specified'))
+                              'if not specified'))
     parser.add_argument('-n', '--numofdays', default=1, type=int,
                         help='Optional: Number of days to search, including the start date.')
 
@@ -371,7 +369,7 @@ def main():
 
     if args.year and args.month and args.day:
         start_date = datetime.combine(date(args.year, args.month, args.day), time())
-        end_date = datetime.combine(start_date + timedelta(days=args.numofdays -1), time(23,59,59))
+        end_date = datetime.combine(start_date + timedelta(days=args.numofdays - 1), time(23, 59, 59))
     towdata = TowingData()
     towdata.write_towing(start_date=start_date, end_date=end_date)
 
